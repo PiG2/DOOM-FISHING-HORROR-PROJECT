581fbba5218f4d0546e4dd8934221ed0d0e9ce2c
Kyledidntdoit <kyle.honeycutt@protonmail.com>
2025-09-22 17:13:47 -0600

    Confined mouse cursor to center screen, also fixed mouse cursor leaving the game window.

--- DOOM-Level-Viewer-main/DOOM-Level-Viewer-main/main.py
+++ DOOM-Level-Viewer-main/DOOM-Level-Viewer-main/main.py
@@ -1,3 +1,5 @@
+from idlelib.autocomplete import TRY_A
+
 from wad_data import WADData
 from settings import *
 import pygame as pg
@@ -13,6 +15,22 @@ class DoomEngine:
     def __init__(self, wad_path='wad/DOOM1.WAD'):
         self.wad_path = wad_path
         self.screen = pg.display.set_mode(WIN_RES, pg.SCALED)
+
+        #Adding screen dimension addition
+        screen_width, screen_height = self.screen.get_size()
+        screen_center = (screen_width / 2, screen_height / 2)
+        pg.mouse.set_pos(screen_center)
+
+        #Adding function to hide mouse cursor while ingame
+        pg.mouse.set_visible(False)
+
+        #Confining mouse cursor to the game window
+        pg.event.set_grab(True)
+        pg.mouse.set_visible(False)
+        pg.mouse.set_pos(screen_center)
+        pg.mouse.get_pos()
+
+
         self.framebuffer = pg.surfarray.array3d(self.screen)
         self.clock = pg.time.Clock()
         self.running = True
